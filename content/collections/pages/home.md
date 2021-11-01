d7b8bd8c4250884c34faff83ce33535d522676c1
Jesper Rørkær <jesperex@gmail.com>
2021-11-01 14:16:27 +0100

    fixed issue with git

---
+++ content/collections/pages/home.md
@@ -0,0 +1,10 @@
+---
+id: home
+blueprint: pages
+title: Home
+template: home
+h1_title: 'Learn From Professionals'
+updated_by: 307a9721-c0ad-4af2-b00a-d49f1532e44b
+updated_at: 1634174892
+h1_paragraph: 'We strive to give you articles with substance that will expand your knowledge of both delopement itself but also how you can succeed with your business.'
+---
---
+++ tailwind.config.js
@@ -0,0 +1,148 @@
+module.exports = {
+  purge: {
+    content: [
+        './resources/**/*.antlers.html',
+        './resources/**/*.blade.php',
+        './content/**/*.md'
+        ]
+    },
+    darkMode: 'media',
+    theme: { 
+        fontFamily: {
+            'stack': [            
+                "Inter",
+                "Roboto",
+                "San Francisco",
+                "Akkurat",
+                "Asap",
+                "Lucida Grande",
+                "-apple-system",
+                "BlinkMacSystemFont",
+                "Segoe UI",
+                "sans-serif"
+            ],
+        },
+        container: {
+            center: true
+        },
+        extend: {
+        "colors": {
+            "primary": "var(--primary)",
+            "primaryLight": "var(--primaryLight)",
+            "primaryFrom": "var(--primaryFrom)",
+            "primaryTo": "var(--primaryTo)",
+            "secondary": "var(--secondary)",
+            "backgroundMain": "var(--backgroundMain)",
+            "backgroundLight": "var(--backgroundLight)",
+            "backgroundLighter": "var(--backgroundLighter)",
+            "backgroundPrimary": "var(--backgroundPrimary)",
+            "backgroundShape": "var(--backgroundShape)",
+            "txtMain": "var(--txtMain)",
+            "txtLight": "var(--txtLight)",
+            "txtLighter": "var(--txtLighter)",
+            "txtInverse": "var(--txtInverse)",
+            "border": "var(--border)",
+            "borderPrimary": "var(--borderPrimary)",
+            "borderDark": "var(--borderDark)",
+            "backendColor": "var(--backendColor)",
+            "frontendColor": "var(--frontendColor)",
+            "designColor": "var(--designColor)",
+            "uxColor": "var(--uxColor)",
+            "businessColor": "var(--businessColor)"
+        },
+        backgroundImage: theme => ({
+            'hero-mesh': "url('/assets/hero-mesh.png')",
+            'rexshop': "url('/assets/rexshop-bg.svg')",
+        }),
+        typography: (theme) => ({
+            light: {
+              css: [
+                {
+                  color: "var(--txtLight)",
+                  a: {
+                    color: "var(--txtMain)",
+                  },
+                  strong: {
+                    color: "var(--txtMain)",
+                  },
+                  'ol > li::before': {
+                    color: "var(--txtLighter)",
+                  },
+                  'ul > li::before': {
+                    backgroundColor: "var(--backgroundLight)",
+                  },
+                  hr: {
+                    borderColor: "var(--borderLight)",
+                  },
+                  blockquote: {
+                    color: "var(--txtLight)",
+                    borderLeftColor: "var(--border)",
+                  },
+                  h1: {
+                    color: "var(--txtMain)",
+                  },
+                  h2: {
+                    color: "var(--txtMain)",
+                  },
+                  h3: {
+                    color: "var(--txtMain)",
+                  },
+                  h4: {
+                    color: "var(--txtMain)",
+                  },
+                  'figure figcaption': {
+                    color: "var(--txtMain)",
+                  },
+                  code: {
+                    color: "var(--txtMain)",
+                  },
+                  'a code': {
+                    color: "var(--txtMain)",
+                  },
+                  pre: {
+                    color: "var(--txtMain)",
+                    backgroundColor: theme('var(--backgroundLight)'),
+                  },
+                  thead: {
+                    color: "var(--txtMain)",
+                    borderBottomColor: "var(--borderLight)",
+                  },
+                  'tbody tr': {
+                    borderBottomColor: "var(--border)",
+                  },
+                },
+              ],
+            },
+          }),
+          animation: {
+            'animate-hover': 'hover 3s linear infinite',
+           },
+           hover: {
+            '0%, 100%': { transform: 'translateY(0deg)' },
+            '50%': { transform: 'translateY(100deg)' },
+          }
+        },
+    },
+    variants: {
+        extend: {
+            translate: ['group-hover', 'group-focus'],
+            filter: ['hover', 'focus'],
+            saturate: ['hover', 'focus'],
+            borderRadius:  ['focus'],
+            blur: ['group-hover', 'group-focus'],
+            grayscale: ['hover', 'focus'],
+            typography: ['dark'],
+            animation: ['motion-safe', 'motion-reduce'],
+            brightness: ['hover'],
+        },
+    },
+    plugins: [
+    require('@tailwindcss/typography'),
+    require("@tailwindcss/forms")({
+        strategy: 'class',
+    }),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-hero-patterns'),
+    ]
+}
