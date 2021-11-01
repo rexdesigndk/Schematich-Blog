cc93bdf80f897387a5a09527b87d669b250dd51b
Srexi <stigrex92@gmail.com>
2021-11-01 09:16:52 +0100

    fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,17 +10,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/site.js', 'public/js')
-
-mix.postCss('resources/css/tailwind.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'),
-    require('postcss-preset-env')({stage: 0})
-])
+mix.js("resources/js/site.js", "public/js")
+    .postCss("resources/css/tailwind.css", "public/css", [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("postcss-nested"),
+    require("postcss-preset-env")({ stage: 0 })
+]);
 
 if (mix.inProduction()) {
-   mix.version();
+    mix.version();
 }
 
 /*
